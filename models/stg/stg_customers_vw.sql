24461dcb1b0c86a7b37dff3adf3515689574901e
Hien Bui <buiminhhien2k@gmail.com>
2026-08-08 01:33:11 +1000

    fix code convention

--- models/stg/stg_customers_vw.sql
+++ models/stg/stg_customers_vw.sql
@@ -1,5 +1,4 @@
-
-SELECT 
-    "customer_id" as customer_id,
-    "customer_zip_code_prefix" as customer_zip_code_prefix
+SELECT
+    "customer_id" AS customer_id,
+    "customer_zip_code_prefix" AS customer_zip_code_prefix
 FROM {{ source('t1_bronze', 'customers') }}
