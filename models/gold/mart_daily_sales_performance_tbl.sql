02d832984a25a94b1684f67681dc99ac859d7c1b
Hien Bui <buiminhhien2k@gmail.com>
2026-08-10 16:51:06 +1000

    create incremental model for mart_daily_sales_performance_tbl

--- models/gold/mart_daily_sales_performance_tbl.sql
+++ models/gold/mart_daily_sales_performance_tbl.sql
@@ -1,3 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['order_purchase_date', 'main_payment_type'],
+        incremental_strategy='merge'
+    )
+}}
+
+WITH cte_last_7_days_orders AS (
+    SELECT *
+    FROM {{ ref('fact_order_shipping_vw') }}
+    WHERE order_purchase_date >= DATEADD(day, -7, CURRENT_DATE)
+    
+)
+
 SELECT
     tbl_b.order_purchase_date,
     tbl_b.main_payment_type,
@@ -8,7 +23,7 @@ SELECT
     sum(tbl_a.quantity) AS items,
     sum(tbl_a.quantity) / count(DISTINCT tbl_a.order_id) AS ipo
 FROM {{ ref('fact_order_items_tbl') }} AS tbl_a
-INNER JOIN {{ ref('fact_order_shipping_vw') }} AS tbl_b
+INNER JOIN cte_last_7_days_orders AS tbl_b
     ON tbl_a.order_id = tbl_b.order_id
 GROUP BY
     tbl_b.order_purchase_date,
