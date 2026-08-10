a54e979901dfc7cc785f4c40b541d1b68d613d0a
Hien Bui <buiminhhien2k@gmail.com>
2026-08-10 16:55:18 +1000

    fix code for sqlfluff

--- models/gold/mart_daily_sales_performance_tbl.sql
+++ models/gold/mart_daily_sales_performance_tbl.sql
@@ -9,19 +9,18 @@
 WITH cte_last_7_days_orders AS (
     SELECT *
     FROM {{ ref('fact_order_shipping_vw') }}
-    WHERE order_purchase_date >= DATEADD(day, -7, CURRENT_DATE)
-    
+    WHERE order_purchase_date >= DATEADD(DAY, -7, CURRENT_DATE)
 )
 
 SELECT
     tbl_b.order_purchase_date,
     tbl_b.main_payment_type,
-    sum(tbl_a.price * tbl_a.quantity) + sum(tbl_a.freight_value) AS total_sale,
-    count(DISTINCT tbl_a.order_id) AS orders,
-    (sum(tbl_a.price * tbl_a.quantity) + sum(tbl_a.freight_value))
-    / (count(DISTINCT tbl_a.order_id)) AS aov,
-    sum(tbl_a.quantity) AS items,
-    sum(tbl_a.quantity) / count(DISTINCT tbl_a.order_id) AS ipo
+    SUM(tbl_a.price * tbl_a.quantity) + SUM(tbl_a.freight_value) AS total_sale,
+    COUNT(DISTINCT tbl_a.order_id) AS orders,
+    (SUM(tbl_a.price * tbl_a.quantity) + SUM(tbl_a.freight_value))
+    / (COUNT(DISTINCT tbl_a.order_id)) AS aov,
+    SUM(tbl_a.quantity) AS items,
+    SUM(tbl_a.quantity) / COUNT(DISTINCT tbl_a.order_id) AS ipo
 FROM {{ ref('fact_order_items_tbl') }} AS tbl_a
 INNER JOIN cte_last_7_days_orders AS tbl_b
     ON tbl_a.order_id = tbl_b.order_id
